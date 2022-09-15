2c5ab5f83a7311ed105a2de8c5ec25c98cbaa908
Your Name <drjpolanco25@yahoo.com>
2022-09-14 20:50:52 -0700

    deploying-2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/wherermybooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/wherermybooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
